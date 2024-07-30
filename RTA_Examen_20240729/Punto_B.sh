9fd4b23da3b1cdb2c52deadd1c37cf393dd1fb95
seek-coder <catrielgatto99@gmail.com>
2024-07-30 01:03:58 -0300

    fix: Punto B

---
+++ RTA_Examen_20240729/Punto_B.sh
@@ -0,0 +1,16 @@
+   86   ----- PUNTO B ------ 
+   87  sudo touch /usr/local/bin/Gatto_check_URL.sh
+   88  sudo vim /usr/local/bin/Gatto_check_URL.sh
+   89  sudo chmod +x /usr/local/bin/Gatto_check_URL.sh
+   90  cd /usr/local/bin
+   91  ./Gatto_check_URL.sh /home/bgatto/UTN-FRA_SO_Examenes/202408/bash_script/Lista_URL.txt 
+   92  sudo vim /usr/local/bin/Gatto_check_URL.sh
+   93  cd /var/log/
+   94  cat status_url.log 
+   95  cd /tmp/head-check/
+   96  ls
+   97  cat Error/cliente/
+   98  cat Error/cliente/noexiste.log 
+   99  ls Error/cliente/
+  100  ls Error/servidor/
+  101  ls ok/
